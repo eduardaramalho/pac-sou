2472ab74958a5727ba6655efad6b99ada9a8f98d
Eduarda Ramalho <eduardacns.ramalho@gmail.com>
2025-08-22 20:05:00 -0300

    initial commit

---
+++ back/pac-sou/src/main/java/com/example/pac_sou/PacSouApplication.java
@@ -0,0 +1,13 @@
+package com.example.pac_sou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacSouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacSouApplication.class, args);
+	}
+
+}
