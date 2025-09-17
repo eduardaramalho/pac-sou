a5bc8fb815c5d0465af9683bff650272071319a6
Eduarda Ramalho <eduardacns.ramalho@gmail.com>
2025-09-17 20:11:42 -0300

    initial commit projeto com front e back

---
+++ back/pac-sou/back/pac-sou/src/test/java/com/example/pac_sou/PacSouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pac_sou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacSouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
